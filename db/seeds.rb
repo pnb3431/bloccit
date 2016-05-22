17ab49decedc795fff4d4f77936cd3f52f468000
pnb3431 <pete.bailen@gmail.com>
2016-05-22 16:44:22 +0000

    Assignment 30 Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,28 @@
 require 'random_data'
 #Create Posts
-50.times do
-    Post.create!(
-        title: RandomData.random_sentence,
-        body: RandomData.random_paragraph
-    )
-end
-posts = Post.all
+#50.times do
+#    Post.create!(
+#        title: RandomData.random_sentence,
+#        body: RandomData.random_paragraph
+#    )
+#end
+#posts = Post.all
 
 #Create Comments
 
-100.times do
-    Comment.create!(
-        post: posts.sample,
-        body: RandomData.random_paragraph
-    )
+#100.times do
+#    Comment.create!(
+#        post: posts.sample,
+#        body: RandomData.random_paragraph
+#    )
+#end
+
+Post.find_or_create_by(title: 'Scarlett', body: 'new body') 
+
+Post.find_or_create_by(title: 'Scarlett') do |post|
+  post.body = 'this is the new body for this one'
 end
+  
 
 puts "Seed finished"
 puts "#{Post.count} post created"
