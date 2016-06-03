5a6c4afa2ae0afb6ca38225efd90ffa1485ec803
pnb3431 <pete.bailen@gmail.com>
2016-06-03 16:11:42 +0000

    checkpoint 52 complete: labels

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,7 @@ class PostsController < ApplicationController
 
     
     if @post.save
+      @post.labels = Label.update_labels(params[:post][:labels])
       flash[:notice] = "Post was saved successfully."
       redirect_to [@topic, @post]
     else
@@ -35,6 +36,7 @@ class PostsController < ApplicationController
     @post.assign_attributes(post_params)
     
     if @post.save
+      @post.labels = Label.update_labels(params[:post][:labels])
       flash[:notice] = "Post was upated successfully."
       redirect_to [@post.topic, @post]
     else
