63cf1af5bc2db7860e48fe15a0b4df70bd33772a
pnb3431 <pete.bailen@gmail.com>
2016-06-06 03:52:57 +0000

    assignment 45 completed

--- spec/models/label_spec.rb
+++ spec/models/label_spec.rb
@@ -1,17 +1,17 @@
  require 'rails_helper'
  
- RSpec.describe Label, type: :model do
+RSpec.describe Label, type: :model do
    let(:topic) { create(:topic) }
    let(:user) { create(:user) }
    let(:post) { create(:post) }
-   let(:label) { Label.create!(name: 'Label') }
-   let(:label2) { Label.create!(name: 'Label2') }
+   let(:label) { create(:label) }
+   let(:label2) { create(:label2) } 
    
    it { is_expected.to have_many :labelings }
    it { is_expected.to have_many(:topics).through(:labelings) }
    it { is_expected.to have_many(:posts).through(:labelings) }
    
-   describe "labelings" do
+    describe "labelings" do
      it "allows the same label to be associated with a different topic and post" do
        topic.labels << label
        post.labels << label
@@ -19,15 +19,14 @@
        topic_label = topic.labels[0]
        post_label = post.labels[0]
        expect(topic_label).to eql(post_label)
-     end
-   end
+      end
+    end
    
-   describe ".update_labels" do
+    describe ".update_labels" do
       it "takes a comma delimited string and returns an array of Labels" do
         labels = "#{label.name}, #{label2.name}"
         labels_as_a = [label, label2]
         expect(Label.update_labels(labels)).to eq(labels_as_a)
       end
     end
-
- end
+end
