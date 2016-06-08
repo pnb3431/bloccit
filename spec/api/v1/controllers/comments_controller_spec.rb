ba4b65b5a672735ce7452bcc8753c82613d29872
pnb3431 <pete.bailen@gmail.com>
2016-06-08 03:23:29 +0000

    assignment 48 complete

---
+++ spec/api/v1/controllers/comments_controller_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+ 
+ RSpec.describe Api::V1::CommentsController, type: :controller do
+   let(:my_user) { create(:user) }
+   let(:my_topic) { create(:topic) }
+   let(:my_post) { create(:post, topic: my_topic, user: my_user) }
+   let(:my_comment) { Comment.create!(body: RandomData.random_paragraph, post: my_post, user: my_user) } 
+   context "unauthenticated user" do
+     before do
+       controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(my_user.auth_token)
+       controller.authenticate_user
+     end
+     
+     it "GET index returns http success" do
+       get :index
+       expect(response).to have_http_status(:success)
+     end
+ 
+     it "GET show returns http success" do
+       get :show, id: my_comment.id
+       expect(response).to have_http_status(:success)
+     end
+     
+     it "GET show returns a comment" do
+         get :show, id: my_comment.id
+         response_hash = JSON.parse response.body
+         expect(response_hash['id']).to eq my_comment.id
+         expect(response_hash['body']).to eq my_comment.body
+     end
+   end
+ 
+   context "unauthorized user" do
+     it "GET index returns http success" do
+       get :index
+       expect(response).to have_http_status(:success)
+     end
+ 
+     it "GET show returns http success" do
+       get :show, id: my_comment.id
+       expect(response).to have_http_status(:success)
+     end
+     
+     it "GET show returns a comment" do
+         get :show, id: my_comment.id
+         response_hash = JSON.parse response.body
+         expect(response_hash['id']).to eq my_comment.id
+         expect(response_hash['body']).to eq my_comment.body
+     end
+   end
+ end
--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -5,6 +5,7 @@ include SessionsHelper
 RSpec.describe TopicsController, type: :controller do
   let(:my_topic) { create(:topic) }
   let(:my_private_topic) { create(:topic, public: false) }
+  
 
   context "guest" do
     describe "GET index" do
